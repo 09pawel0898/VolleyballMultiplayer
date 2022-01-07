3d60ef2a9dd11c9069deda7947ea0e851e490ee7
09pawel0898 <pawel_2014@interia.eu>
2022-01-07 17:53:39 +0100

    [mainmenu] update function refactor

--- Client/src/states/mainMenuState.py
+++ Client/src/states/mainMenuState.py
@@ -1,9 +1,15 @@
-import pygame
-
 from src.core.states.state import *
 from src.core.widgets.button import Button
 from src.core.widgets.textbox import TextBox
 from src.core.util.utilis import lerp, start_delayed
+
+class UIAnimState(Enum):
+    LoginPanelSlideIn = 1
+    LoginPanelSlideOut = 2
+    LoginPanelVisible = 3
+    RegisterPanelSlideIn = 4
+    RegisterPanelSlideOut = 5
+    RegisterPanelVisible = 6
 
 class MainMenuState(State):
     def __init__(self, context, state_manager):
@@ -13,6 +19,7 @@ class MainMenuState(State):
         self.bInputEnabled = False
         self.bLogoAnimEnabled = False
         self.bLogoAnimGoingDown = True
+        self.UIAnimState = UIAnimState.LoginPanelSlideIn
 
     def _init_resources(self):
         self.context.texture_manager.load_resource(TextureID.BackgroundLayer0,
@@ -101,29 +108,26 @@ class MainMenuState(State):
     def _enable_logo_anim(self):
         self.bLogoAnimEnabled = True
 
-    def _on_update(self, dt: float) -> None:
-        # update widgets
-        self.widget_manager.update_widgets()
-
-        #update login_panel animation
+    def _update_login_panel_anim(self):
         button_sign_in = self.widget_manager.get_widget("ButtonSignIn")
         button_sign_up = self.widget_manager.get_widget("ButtonSignUp")
         login_input_box = self.widget_manager.get_widget("LoginInputBox")
         password_input_box = self.widget_manager.get_widget("PasswordInputBox")
         if self.login_panel.position.y > 25:
-            offset = self.login_panel_init_pos - lerp(self.login_panel.position.y,15.0,0.007)
-            self.login_panel.set_position(0,self.login_panel_init_pos - offset)
-            button_sign_in.set_position(356,self.temp_initial_widgets_ys[0] - offset)
-            button_sign_up.set_position(475,self.temp_initial_widgets_ys[1] - offset)
-            login_input_box.set_position(485,self.temp_initial_widgets_ys[2] - offset)
-            password_input_box.set_position(485,self.temp_initial_widgets_ys[3] - offset)
+            offset = self.login_panel_init_pos - lerp(self.login_panel.position.y, 15.0, 0.007)
+            self.login_panel.set_position(0, self.login_panel_init_pos - offset)
+            button_sign_in.set_position(356, self.temp_initial_widgets_ys[0] - offset)
+            button_sign_up.set_position(475, self.temp_initial_widgets_ys[1] - offset)
+            login_input_box.set_position(485, self.temp_initial_widgets_ys[2] - offset)
+            password_input_box.set_position(485, self.temp_initial_widgets_ys[3] - offset)
         elif not self.bInputEnabled:
-            button_sign_in.set_initial_pos(Vec2(356,356))
-            button_sign_up.set_initial_pos(Vec2(475,418))
+            button_sign_in.set_initial_pos(Vec2(356, 356))
+            button_sign_up.set_initial_pos(Vec2(475, 418))
             self.bInputEnabled = True
-            start_delayed(0.6,self._enable_logo_anim)
+            self.UIAnimState = UIAnimState.LoginPanelVisible
+            start_delayed(0.6, self._enable_logo_anim)
 
-        #update logo animation
+    def _update_logo_anim(self):
         if self.bLogoAnimEnabled:
             if self.bLogoAnimGoingDown:
                 if self.logo.position.y < 12.0:
@@ -136,11 +140,23 @@ class MainMenuState(State):
                 else:
                     self.bLogoAnimGoingDown = True
 
-        #update clouds
+    def _update_clouds(self):
         for clouds in self.cloudsPool:
             clouds.move(Vec2(0.3, 0))
             if clouds.position.x > self.context.window.get_width():
                 clouds.set_position(-self.context.window.get_width(), 0)
+
+    def _update_ui(self):
+        if self.UIAnimState == UIAnimState.LoginPanelVisible or self.UIAnimState == UIAnimState.RegisterPanelVisible:
+          pass
+        elif self.UIAnimState == UIAnimState.LoginPanelSlideIn or self.UIAnimState == UIAnimState.LoginPanelSlideOut:
+            self._update_login_panel_anim()
+
+    def _on_update(self, dt: float) -> None:
+        self.widget_manager.update_widgets()
+        self._update_ui()
+        self._update_logo_anim()
+        self._update_clouds()
 
 
 
