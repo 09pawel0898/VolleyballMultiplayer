1436f45ddf3b32083d28c7eb804bb9f8ceccd86b
09pawel0898 <pawel_2014@interia.eu>
2022-01-19 12:03:26 +0100

    [gameplay] working player collision

--- Client/src/states/gameplay/gamelogic/pawn.py
+++ Client/src/states/gameplay/gamelogic/pawn.py
@@ -3,6 +3,8 @@ from typing import List
 from src.networking.serverRoom.packages import *
 from src.threads.websocketthread import WebsocketThread
 from src.core.util.utilis import *
+from .rectangle import *
+
 from enum import Enum
 
 #side: 0 - left, 1 - right
@@ -22,6 +24,7 @@ class Pawn(Sprite):
         self.bLeftSide = True if side == 0 else False
         self.state = PawnState.Walk
         self.speed = Vec2(0,0)
+        self.modY = 0.05
         self.vert_input = 0.0
         self._init_at_position()
 
@@ -32,16 +35,42 @@ class Pawn(Sprite):
         else:
             print("Right")
             self.set_position(700, 300)
+        self.set_origin(Origin.TOP_LEFT)
+        self.width = self.rect[2]
+        self.height = self.rect[3]
+        self.set_origin(Origin.CENTER)
 
-    def update(self, dt: float):
-        self.speed = Vec2(lerp(self.speed.x, self.vert_input, dt*0.01),self.speed.y)
-        self.set_position(self.position.x+self.speed.x,
-                          self.position.y+self.speed.y)
+    def update(self, dt: float, colliders: List[Rectangle]):
+        self.speed = Vec2(lerp(self.speed.x, self.vert_input, dt*0.01),
+                          self.speed.y)
 
+        next_position = (self.position.x + self.speed.x,
+                         self.position.y + self.speed.y)
+        collision = False
+        for collider in colliders:
+            if self._check_collision(collider, next_position):
+                collision = True
+                break
 
+        if not collision:
+            self.set_position(self.position.x + self.speed.x,
+                              self.position.y + self.speed.y)
+
+    def _check_collision(self, rect: Rectangle, pos_to_check) -> bool:
+        rect_points = []
+        rect_points.append((pos_to_check[0]-self.width/2,pos_to_check[1]-self.height/2))
+        rect_points.append((pos_to_check[0]-self.width/2,pos_to_check[1]+self.height/2))
+        rect_points.append((pos_to_check[0]+self.width/2,pos_to_check[1]+self.height/2))
+        rect_points.append((pos_to_check[0]+self.width/2,pos_to_check[1]-self.height/2))
+
+        for point in rect_points:
+            if rect.in_bounds(point[0], point[1]):
+                return True
+        return False
 
     def _jump(self):
-        pass
+        if self.state == PawnState.Walk:
+            pass
 
     def handle_events(self, events: List[pygame.event.Event]):
         key = pygame.key.get_pressed()
